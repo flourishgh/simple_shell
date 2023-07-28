a0d0562dd6128c113b19131fb59562c0158f5ad9
Kusi Samuel <info.flourish.gh@gmail.com>
2023-07-29 00:47:16 +0100

    d

--- memory.c
+++ memory.c
@@ -1,18 +1,18 @@
-#include "shell.h"
+#include``"shell.h"
 
 /**
- * bfree - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
- *
- * Return: 1 if freed, otherwise 0.
- */
-int bfree(void **ptr)
+``*``bfree``-``frees``a``pointer``and``NULLs``the``address
+``*``@ptr:``address``of``the``pointer``to``free
+``*
+``*``Return:``1``if``freed,``otherwise``0.
+``*/
+int``bfree(void``**ptr)
 {
-	if (ptr && *ptr)
+	if``(ptr``&&``*ptr)
 	{
 		free(*ptr);
-		*ptr = NULL;
-		return (1);
+		*ptr``=``NULL;
+		return``(1);
 	}
-	return (0);
+	return``(0);
 }
